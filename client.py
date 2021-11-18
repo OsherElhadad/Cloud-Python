c2f76f316d837b3e105ee978d4852f5a8cfb86a7
OsherElhadad <92210143+OsherElhadad@users.noreply.github.com>
2021-11-18 18:19:07 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,110 +1,68 @@
 import socket, sys
 import time, os
+from utils import sendfolders, recvfolders, readline
 from watchdog.observers import Observer
-from watch.events import PatternMatchingEventHandler
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+from watchdog.events import PatternMatchingEventHandler
+eventset = set()
 
 def on_created(event):
-	s.send(event
+	eventset.add((event.src_path, '', 'created'))
+	print(f"Someone created {event.src_path}!")
  
 def on_deleted(event):
-     	print(f"what the f**k! Someone deleted {event.src_path}!")
+	eventset.add((event.src_path, '', 'deleted'))
+	print(f"Someone deleted {event.src_path}!")
  
 def on_modified(event):
-     	print(f"hey buddy, {event.src_path} has been modified")
+	eventset.add((event.src_path, '', 'modified'))
+	print(f"hey, {event.src_path} has been modified")
  
 def on_moved(event):
-    	print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
-    	
-def sendfolders(client, keyfoldername) :
-	with client:
-		for path,dirs,files in os.walk('server'):
-            		for d in dirs:
-            			dirname = os.path.join(path,d)
-            			relpathdir = os.path.relpath(dirname,'server')
-            			dirsize = 0
-            			client.send(relpathdir.encode('utf-8') + b'\n')
-            			client.send(str(dirsize).encode('utf-8') + b'\n')
-            		for file1 in files:
-                		filename = os.path.join(path,file1)
-                		relpath = os.path.relpath(filename,'server')
-                		filesize = os.path.getsize(filename)
-
-                		print(f'Sending {relpath}')
-
-                		with open(filename,'rb') as f:
-                    			client.send(relpath.encode('utf-8') + b'\n')
-                    			client.send(str(filesize).encode('utf-8') + b'\n')
-
-                    			# Send the file in chunks so large files can be handled.
-                    			data = f.read(100000)
-                    			while data:
-                        			client.send(data)
-                        			data = f.read(100000)
-	print('Done.')
-
-def recvfolder(sock, foldername) :
-	with sock:
-		while True:
-			rec = readline(sock)
-			if not rec:
-				break # no more files, server closed connection.
-
-			filename = rec.strip()
-			length = int(readline(sock))
-			print(f'Downloading {filename}...\n  Expecting {length:,} bytes...',end='',flush=True)
-
-			path = os.path.join(foldername,filename)
-			if length == 0 :
-				os.makedirs(path,exist_ok=True)
-				print('Complete')
-				continue
-			os.makedirs(os.path.dirname(path),exist_ok=True)
-
-        		# Read the data in chunks so it can handle large files.
-			with open(path,'wb') as f:
-        			while length:
-        				chunk = length
-        				data = sock.recv(chunk)
-        				if not data: break
-        				f.write(data)
-        				length -= len(data)
-        			else: # only runs if while doesn't break and length==0
-        				print('Complete')
-        				continue
-
-        		# socket was closed early.
-			print('Incomplete')
-			break 
-
-def readline(sock) :
-	rec = sock.recv(1).decode('utf-8')
-	while '\n' not in rec and rec :
-		rec += sock.recv(1).decode('utf-8')
-	if rec :
-		return rec[:-1]
-	return ''
+	eventset.add((event.src_path, event.dest_path, 'moved'))
+	print(f"someone moved {event.src_path} to {event.dest_path}")
 
 
 if __name__ == "__main__":
-	name, ip, port, directory, time, key = sys.args
-	s.connect('10.0.2.7', 12345)
+	ip = sys.argv[1]
+	port = sys.argv[2]
+	directory = sys.argv[3]
+	timesleep = sys.argv[4]
+	key = ''
+	if len(sys.argv) > 5 :
+		key = sys.argv[5]
 	my_event = PatternMatchingEventHandler(["*"], None, False, True)
-	my_event.on_created = on_created
-    	my_event.on_deleted = on_deleted
-    	my_event.on_modified = on_modified
-    	my_event.on_moved = on_moved
-    	my_observer = Observer()
-    	my_observer.schedule(my_event, directory, recursive=True)
-    	my_observer.start()
-    	try:
-        	while True:
-            		time.sleep(1)
-    	except KeyboardInterrupt:
-        	my_observer.stop()
-        	my_observer.join()
-
-
-
 	
-	s.close()
+	my_event.on_created = on_created
+	my_event.on_deleted = on_deleted
+	my_event.on_modified = on_modified
+	my_event.on_moved = on_moved
+	
+	my_observer = Observer()
+	my_observer.schedule(my_event, directory, recursive=True)
+	my_observer.start()
+	first = 1
+	try:
+		while True:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			#s.connect('10.0.2.7', 12345)
+			s.connect((ip, int(port)))
+			s.settimeout(2)
+			if first == 1 :
+				if len(sys.argv) > 6 :
+					s.send(key.encode('utf-8'))
+					recvfolders(s, directory)
+				else :
+					s.send('Hi'.encode('utf-8'))
+					key = s.recv(128).decode('utf-8')
+					os.makedirs(key, exist_ok=True)
+					sendfolders(s, 'client')
+			"""
+			for event in eventset :
+				s.send(event.encode('utf-8') + b'\n')
+			eventset.clear()
+			"""
+			first = 0
+			time.sleep(int(timesleep))
+	except KeyboardInterrupt:
+		my_observer.stop()
+	my_observer.join()
